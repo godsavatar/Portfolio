28b52bc06609e65938e50db37a4945c15f9e8565
Daniel <57568959+godsavatar@users.noreply.github.com>
2023-08-26 09:15:41 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+//nav-bar js
+let navMenu = document.querySelector(".nav-menu");
+let menu = document.querySelector(".menu");
+
+menu.addEventListener("click", ()=>{
+navMenu.classList.toggle("active");
+menu.classList.toggle("active");
+
+})
+//
